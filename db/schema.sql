a98976f4619b9c01cf4a920b29511549f6355db4
Julio C. MW11 <sysa.mail@gmail.com>
2022-12-23 20:40:24 -0600

    se agrega a eschema tabla comment

--- db/schema.sql
+++ db/schema.sql
@@ -14,11 +14,23 @@ create table user
 
 create table post
 (
-    id      integer not null auto_increment,
-    title   varchar(100) not null,
-    content varchar(200) not null,
+    id          integer not null auto_increment,
+    title       varchar(256) not null,
+    content     text not null,
     createdat   datetime not null default CURRENT_TIMESTAMP,
-    user_id integer not null,
+    user_id     integer not null,
+    FOREIGN KEY (user_id) REFERENCES user(id),
+    primary key (id)
+);
+
+create table comment
+(
+    id          integer not null auto_increment,
+    comment     varchar(256) not null,
+    createdat   datetime not null default CURRENT_TIMESTAMP,
+    post_id     integer not null,
+    user_id     integer not null,
+    FOREIGN KEY (post_id) REFERENCES post(id),
     FOREIGN KEY (user_id) REFERENCES user(id),
     primary key (id)
 );
