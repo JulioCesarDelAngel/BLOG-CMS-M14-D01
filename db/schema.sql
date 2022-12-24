df5ba2f3fe9d069cc0efd6f0fcc3596c68a82114
Julio C. MW11 <sysa.mail@gmail.com>
2022-12-23 20:12:04 -0600

    se agrega a eschema tabla user

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,13 @@ DROP DATABASE IF EXISTS BLOG_CMS_db;
 
 CREATE DATABASE BLOG_CMS_db;
 
+USE BLOG_CMS_db;
+
+create table user
+(
+    id          integer not null auto_increment,
+    username    varchar(50) not null,
+    password    varchar(50) not null,
+    primary key (id)
+);
+
