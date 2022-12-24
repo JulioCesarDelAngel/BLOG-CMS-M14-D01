830cd9340f40df6d8eb6cc2c2151cc9fb7c27e27
Julio C. MW11 <sysa.mail@gmail.com>
2022-12-24 15:07:08 -0600

    test de relaciones de modelo

--- models/index.js
+++ models/index.js
@@ -7,11 +7,6 @@ User.hasMany(Comment, { foreignKey : 'user_id' });
 
 Post.belongsTo(User, { foreignKey : 'user_id'});
 Post.hasMany(Comment , { foreignKey : 'post_id' , onDelete : 'CASCADE'});
-
 Comment.belongsTo(User, { foreignKey : 'user_id'});
-Comment.belongsTo(Post, { foreignKey : 'post_id'});
-
-User.belongsToMany(Post, { through : Comment, foreignKey : 'user_id'});
-
 
 module.exports = { User, Post, Comment}; 
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const sequelize = require('./config/connection');
+//test relaciones 
+//const { User, Post, Comment} = require ('./models');
+const { userInfo } = require('os');
 
 const app = express();
 const PORT = process.env.PORT || 3001; //Necesario para el despliegue en Heroku 
@@ -24,5 +27,32 @@ app.use(express.static(path.join(__dirname,'public')));
 
 
 sequelize.sync( {force : false} ).then( ()=> {
+
     app.listen(PORT, () => console.log( ` Now listening on port ${PORT} `));
+    //testData();
+
 });
+
+
+/* async function  testData(){
+    console.log('Usuarios y sus posts:')
+    const userData = await User.findByPk(1, { include : [ { model : Post} ] });
+    console.log('Usuarios y sus posts:' , userData );
+
+    console.log('Usuarios y sus comments:')
+    const userCommentData = await User.findByPk(1, { include : [ { model : Comment} ] });
+    console.log('Usuarios y sus comments:' , userCommentData  );
+
+    console.log('Post ,su owner y sus comentarios:')
+    const PostUserData = await Post.findAll( { include : [ { model : User,
+                                                include : [ { model : Comment} ]}
+                                             ]});
+    console.log('Post ,su owner y sus comentarios:' , PostUserData   );
+
+    console.log('Post ,su owner y sus comentarios y su owner:')
+    const PostUserDataComment = await Post.findAll( { include : [ { model : User,
+                                                include : [ { model : Comment, include : [{ model : User}] } ]}
+                                             ]});
+    console.log('Post ,su owner y sus comentarios y su owner:' , PostUserDataComment    );
+
+} */
