450acaa77b1eb8f3b39a6c18b278e8d3109db8c1
Julio C. MW11 <sysa.mail@gmail.com>
2023-01-04 11:09:02 -0600

    se agrega validaciones y mensajes en login y registro, se agrega parcial de mensajes, se soluciona bug de css

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,6 +7,7 @@ router.get('/login', async (request, response) => {
 
 router.post('/login', async (request, response) => {
     //response.send('post Login ok request:Usr/psw'+ `${request.body.inputUser}` +'/'+ `${request.body.inputPassword}`)
+    let messages = [];
     try {
         const userDb = await User.findOne({
             where :{
@@ -16,18 +17,18 @@ router.post('/login', async (request, response) => {
         
         if (!userDb)
         {
-            response
+            messages.push({ message: "Usuario no encontrado." });
+            return response.render('login', {typeLogin:true, messages});            
+/*             response
             .status(400)
             .json({message:'Usuario no encontrado!'});
-            return;
-        }
+            return;*/
+         }
 
         const validPassword = await userDb.checkPassword(request.body.inputPassword);
         if (!validPassword) {
-            response
-              .status(400)
-              .json({ message: 'password incorrecto. intente de nuevo!' });
-            return;
+            messages.push({ message: "Password incorrecto." });
+            return response.render('login', {typeLogin:true, messages});            
           }
         else{
             request.session.save(() => {
@@ -49,6 +50,47 @@ router.post('/login', async (request, response) => {
 router.get('/singup', async (request, response) => {
         response.render('login', {typeLogin:false});
     })
+
+router.post('/singup', async (request, response) => {
+    //response.send('post singup ok request:Usr/psw'+ `${request.body.inputUser}` +'/'+ `${request.body.inputPassword}`)
+    let messages = [];
+    try {
+
+        const exists = await User.findOne({
+            where :{
+                username : request.body.inputUser
+            }
+        });
+        
+        if (!!exists)
+        {
+            messages.push({ message: `El usuario ${request.body.inputUser} ya se encuentra registrado.`});
+            return response.render('login', {typeLogin:true, messages});
+         }
+
+    }
+    catch ( error ){
+        console.log(error);
+        response.status(500).json(error);
+    }
+
+    const newUser = await User.create({
+                                            username: request.body.inputUser,
+                                            password: request.body.inputPassword
+                                        });
+                                        console.log('nuevo usuaro', newUser);
+                                        console.log('nuevo !!', !!newUser);
+    
+    if (!!newUser){
+        request.session.save(() => {
+            request.session.loggedIn = true;
+            request.session.username = newUser.username;
+            request.session.userid = newUser.id;
+            console.log('Registro de nuevo usuario y su session', request.session.cookie);
+            response.redirect('/');
+        });        
+    }
+})
 
 
 router.get('/logout', async (request, response) => {
