a4914ca1c7902b0397cd32dd6a2bf353e2954ae8
Julio C. MW11 <sysa.mail@gmail.com>
2023-01-05 04:21:46 -0600

    se agrega helper para formato de fechas y se aplica a vistas

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,6 +9,11 @@ module.exports = {
     },
     is_equal : (val1, val2) => {
         return val1 === val2;
-    }
+    },
+    format_date: (date) => {
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
+          new Date(date).getFullYear() + 5
+        }`;
+      }
   };
   
