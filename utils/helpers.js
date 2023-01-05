7d817f77a580fdbd014baddb510b7a8b76a49f72
Julio C. MW11 <sysa.mail@gmail.com>
2023-01-05 03:50:26 -0600

    se agrega helper para editar comentarios del owner

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,6 +6,9 @@ module.exports = {
     filter_commentPost : (data, postId) => {
         const filter = data.filter(row => row.post_id == postId);        
         return filter;
+    },
+    is_equal : (val1, val2) => {
+        return val1 === val2;
     }
   };
   
