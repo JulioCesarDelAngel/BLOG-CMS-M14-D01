02697da422267fafa1f0b24352d1e710b1ab1501
Julio C. MW11 <sysa.mail@gmail.com>
2023-01-05 03:51:24 -0600

    se agregar rutas de CRUD de comentarios

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const {Post , User, Comment } = require('../models');
+const appAuth = require('../utils/appAuth');
 
 router.get('/', async (request, response) => {
     postData = await Post.findAll({
@@ -19,7 +20,7 @@ router.get('/', async (request, response) => {
     }); 
 
      commentData = await Comment.findAll({
-        attributes:['id', 'post_id', 'comment', 'createdat'], 
+        attributes:['id', 'post_id', 'comment', 'createdat', 'user_id'], 
         include : [ { model: User, attributes : ['username']}]
     })
 
@@ -33,10 +34,115 @@ router.get('/', async (request, response) => {
     console.log('session user id: ', request.session.userid); */
 
     const loggedIn = request.session.loggedIn === undefined ? false : true;
+    const userId = loggedIn ? request.session.userid : 0;
+    response.render('index', {postRows, commentRows, loggedIn: loggedIn, userId : userId});
 
-    response.render('index', {postRows, commentRows, loggedIn: loggedIn});
 })
 
+
+//render formulario de alta
+router.get('/newcomment/:id', async (request, response) => {        
+    const loggedIn = !!request.session.loggedIn ;
+    response.render('comments', {loggedIn: loggedIn, newComment:true, postId:request.params.id});
+    return;
+})
+
+
+//crear comentario del post
+router.post('/comment/:id', appAuth, async (request, response) => {
+    console.log(`alta de nuevo comentario ${request.body.comment} post_id:${request.params.id} user_id : ${request.session.userid}`);
+
+    try {
+        const newComment = await Comment.create({            
+            comment : request.body.inputComment,
+            post_id : request.params.id,
+            user_id : request.session.userid
+        });
+        response.redirect('/')
+
+    }
+    catch ( error ) {
+        response.status(500).json(error);
+    }
+
+});
+    
+//render para editar el post
+router.get('/comment/:id', appAuth, async (request, response) => { 
+
+//    response.render('comments', {loggedIn: loggedIn, newComment:true, postId:request.params.id});
+     
+    try {        
+        const rowComment = await Comment.findOne({
+            where :{
+                id : request.params.id
+            }
+        });
+
+        if (!!rowComment){
+            const loggedIn = !!request.session.loggedIn ;
+            response.render('comments', {loggedIn: loggedIn, newComment:false, postId : rowComment.post_id, id:rowComment.id, comment : rowComment.comment});        
+        }
+
+    }
+    catch ( error ) {
+        console.log(error);
+        response.status(500).json(error);
+    }
+   return; 
+})
+
+router.put('/comment/:id', appAuth, async (request, response) => { 
+    try {
+        const rowComment = await Comment.update({
+            comment : request.body.comment
+        },
+        {
+            where : {
+                id : request.params.id
+            }
+        }
+        );
+
+        console.log(`update row :`, !rowComment)
+
+        if (!rowComment){
+            response.status(404).json({code : 404, message : 'No se encontro registro.', data : []});            
+        }
+        else{
+            response.status(200).json({code : 200, message : 'Registro actualizado.', data : [rowComment]});            
+        }
+
+    }
+    catch ( error ){
+        response.status(500).json(error);
+    }
+    return;
+})
+
+router.delete('/comment/:id', appAuth, async (request, response) => { 
+    try {
+        const rowComment = await Comment.destroy({
+            where : {
+                id : request.params.id
+            }
+        });
+
+        console.log(`delete row :`, !rowComment)
+
+        if (!rowComment){
+            response.status(404).json({code : 404, message : 'No se encontro registro.', data : []});            
+        }
+        else{
+            response.status(200).json({code : 200, message : 'Registro de comentario eliminado.', data : [rowComment]});            
+        }
+
+    }
+    catch ( error ){
+        response.status(500).json(error);
+    }
+    return;
+})
 
 
 module.exports = router;
